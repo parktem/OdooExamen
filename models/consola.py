209299487be0fa21e9d07e99742818d34846f85e
parktem <alejandromayo98@gmail.com>
2019-02-25 09:13:33 +0100

    Tercer commit

--- models/consola.py
+++ models/consola.py
@@ -1,6 +1,7 @@
 from odoo import models,fields, api
 
 class consola(models.Model):
+    _inherit = 'base.entidad'
     _name = 'examen.consola'
     name = fields.Char(string="Nombre Consola")
     videojuego_ids = fields.One2many("examen.videojuego", "consola_id", string="videojuegos")
